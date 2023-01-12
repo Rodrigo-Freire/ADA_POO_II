542c6057d57980667635beb6d98fe1486c3d80be
Rodrigo <rodrigo.freire@msn.com>
2023-01-11 22:41:14 -0300

    CRIANDO A CLASS PESSOA E ALUNO

---
+++ Reviao_de_POO/Model/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reviao_de_POO.Model
+{
+    public class Aluno : Pessoa 
+    {
+        public Aluno(string nome, int idade) : base(nome, idade)
+        {
+
+        }
+
+        public void IrParaEscola()
+        {
+            Console.WriteLine("Indo para escola");
+        }
+
+    }
+}
---
+++ Reviao_de_POO/Model/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reviao_de_POO.Model
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }    
+        public int idade { get; set; }
+
+        public Pessoa(string nome, int idade) 
+        {
+            this.nome = nome;
+            this.idade = idade; 
+        }
+
+
+        public void Cumprimentar ()
+        {
+            Console.WriteLine($"Olá meu nome é: "+ nome);
+        }
+
+        public void DizerIdade ()
+        {
+            Console.WriteLine("Minha idade é: " + idade);
+        }
+    }
+}
