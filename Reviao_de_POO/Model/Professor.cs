3d915500dbcd937f4be23b32e9426947912580d4
Rodrigo <rodrigo.freire@msn.com>
2023-01-12 12:38:22 -0300

    ADICIONANDO A CLASS PROFESSOR

---
+++ Reviao_de_POO/Model/Professor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reviao_de_POO.Model
+{
+    public class Professor : Pessoa
+    {
+        string assunto { get; set; }
+        public Professor(string nome, int idade) : base(nome, idade) 
+        {
+
+        }
+
+        public void explicaAssunto()
+        {
+            Console.WriteLine("Hoje vamos falar sobre: " + assunto);
+        }
+    }
+}
