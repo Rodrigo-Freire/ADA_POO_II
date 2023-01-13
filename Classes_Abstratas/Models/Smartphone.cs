918e68a6d0e5ba68fefcc39f05819e924f6005af
Rodrigo <rodrigo.freire@msn.com>
2023-01-13 13:37:47 -0300

    INICANDO EXERCÍCIO DE CLASSES ABSTRATAS E CRIANDO A CLASS SMARTPHONE

---
+++ Classes_Abstratas/Models/Smartphone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Abstratas.Models
+{
+    public abstract class Smartphone
+    {
+        public string numero { get; set; }
+        public string modelo { get; set; }
+        public long imei { get; set; }
+        public int memoria { get; set; }
+
+        public void Ligar(long numerodiscado)
+        {
+            Console.WriteLine($"Chamando... {numerodiscado}");
+        }
+
+        public void recebeLigacao()
+        {
+            Random r = new Random();
+            int ddd = r.Next(11, 99);      
+            int num2 = r.Next( 2000, 9999 );
+            int num3 = r.Next(2000, 9999);
+
+            Console.WriteLine($"Recebendo ligação... ({ddd}) 9{num2}-{num3} ");
+        }
+
+        public virtual string instalarApp(string nomeLoja)
+        {
+            return "Acessando " + nomeLoja;
+        }
+
+    }
+}
---
+++ Classes_Abstratas/Program.cs
@@ -0,0 +1,12 @@
+﻿using Classes_Abstratas.Models;
+
+namespace Classes_Abstratas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
