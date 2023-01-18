0bab5dc6dd8d9af6ef282d2af34f2808bb03957e
Rodrigo <rodrigo.freire@msn.com>
2023-01-18 17:03:37 -0300

    Criando o Projeto Treinando interfaces com Animais

---
+++ Treinando interfaces com Animais/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Treinando_interfaces_com_Animais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
