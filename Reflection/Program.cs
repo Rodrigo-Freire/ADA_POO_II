9a67c295f1d316ed1738b5545b05fea9c15cf0d3
Rodrigo <rodrigo.freire@msn.com>
2023-01-18 12:39:33 -0300

    Parte 1 do Exercício

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student = new Student();
+            DisplayPublicProperties(typeof(Student));
+        }
+        
+        public static void DisplayPublicProperties(Type type)
+        {
+            var properties = type.GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+            foreach (var property in properties)
+            {
+                Console.WriteLine(property.Name);
+            }
         }
     }
 }
