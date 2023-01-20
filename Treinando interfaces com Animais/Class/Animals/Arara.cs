dc5622e420a2f76e3b4f6b346d25201a7250403b
Rodrigo <rodrigo.freire@msn.com>
2023-01-20 17:11:38 -0300

    Criando as Class Arara e Dragao de Comofo

---
+++ Treinando interfaces com Animais/Class/Animals/Arara.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treinando_interfaces_com_Animais.Interfaces;
+
+namespace Treinando_interfaces_com_Animais.Class.Animals
+{
+    public class Arara : Bird, IFly, IOvipro
+    {
+        public Arara(string name, DateTime birthDate, char genre, int age) : base(name, birthDate, genre, age)
+        {
+        }
+
+        public string maximumAltitude
+        {
+            get { return "98 pés"; }
+            set { maximumAltitude= value; }
+        }
+        public double flightSpeed
+        {
+            get { return 56; }
+            set { flightSpeed = value; }
+        }
+
+        public void Flying()
+        {
+            Console.WriteLine($"A {Name} consegue voar a uma altitude de {maximumAltitude} a uma velocidade de {flightSpeed}Km/h");
+        }
+
+        public void Hatch()
+        {
+            Console.WriteLine($"A {Name} choca os ovos por 27 a 30 dias.");
+        }
+
+        public void Lay()
+        {
+            Console.WriteLine($"A {Name} consgue botar até 3 ovos.");
+        }
+    }
+}
---
+++ Treinando interfaces com Animais/Class/Animals/Dragao_De_Comodo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treinando_interfaces_com_Animais.Interfaces;
+
+namespace Treinando_interfaces_com_Animais.Class.Animals
+{
+    public class DragaoDeComodo : Reptile, IOvipro
+    {
+        public DragaoDeComodo(string name, DateTime birthDate, char genre, int age) : base(name, birthDate, genre, age)
+        {
+        }
+
+        public void Hatch()
+        {
+            Console.WriteLine($"O {Name} incuba os ovos por 8 a 9 meses.");
+        }
+
+        public void Lay()
+        {
+            Console.WriteLine($"O {Name} consgue botar até 26 ovos.");
+        }
+    }
+}
