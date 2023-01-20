46056e3e4b87280cf0eb2205f03c4ea2e120261c
Rodrigo <rodrigo.freire@msn.com>
2023-01-20 16:45:50 -0300

    Criando a Class Cisne

---
+++ Treinando interfaces com Animais/Class/Animals/Cisne.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treinando_interfaces_com_Animais.Interfaces;
+
+namespace Treinando_interfaces_com_Animais.Class.Animals
+{
+    public class Cisne : Bird, IFly, Aquatic, IOvipro
+    {
+        public Cisne(string name, DateTime birthDate, char genre, int age) : base(name, birthDate, genre, age)
+        {
+
+        }
+
+        public string maximumAltitude 
+            { get { return "10 pés"; }
+              set { maximumAltitude = value; } 
+            }
+        public double flightSpeed
+        {
+            get { return 135; }
+            set { flightSpeed = value; }
+        }
+
+        public bool Dips()
+        {
+            return false;
+        }
+
+        public void Flying()
+        {
+            Console.WriteLine($"O {Name} consegue voar a uma altitude de {maximumAltitude} a uma velocidade de {flightSpeed}Km/h");
+        }
+
+        public bool Freshwater()
+        {
+            return true;
+        }
+
+        public void Hatch()
+        {
+            Console.WriteLine($"O {Name} choca os ovos por 34 a 40 dias.");
+        }
+
+        public void Lay()
+        {
+            Console.WriteLine($"O {Name} consgue botar até 8 ovos.");
+        }
+
+        public bool LiveOnLand()
+        {
+            return false;
+        }
+    }
+}
--- Treinando interfaces com Animais/Program.cs
+++ Treinando interfaces com Animais/Program.cs
@@ -6,6 +6,10 @@ namespace Treinando_interfaces_com_Animais
     {
         static void Main(string[] args)
         {
+            
+            Console.ForegroundColor= ConsoleColor.DarkBlue;
+            
+
             string Name = "Leão";
             DateTime birthDate = new DateTime(2020, 01, 01);
             char genre = 'M';
@@ -17,10 +21,22 @@ namespace Treinando_interfaces_com_Animais
                 $"\nNascido em {leao.birthDate.ToString()}" +
                 $"\nDo genero {leao.genre}" +
                 $"\nTenho {leao.age.ToString()} anos" +
-                $"\nAs fêmas da minha espécie tem {leao.qtyBoobs} mamas");
+                $"\nAs fêmas da minha espécie tem {leao.qtyBoobs} mamas\n");
+
+            Name = "Cisne";
+            Cisne cisne = new Cisne(Name, birthDate, genre, age);
+            Console.WriteLine($"Eu sou um {cisne.Name}" +
+                $"\nNascido em {cisne.birthDate.ToString()}" +
+                $"\nDo genero {cisne.genre}" +
+                $"\nTenho {cisne.age.ToString()} anos\n");
+
+            cisne.Flying();
+            cisne.Lay();
+            cisne.Hatch(); 
+            
 
 
 
-    }
+        }
     }
 }
